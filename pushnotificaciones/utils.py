0e38572a15d759e9b028a057f078fd4a79528236
Eliseo <eliseonop@gmail.com>
2025-08-20 09:54:09 -0500

    First clean commit

--- pushnotificaciones/utils.py
+++ pushnotificaciones/utils.py
@@ -1,17 +1,17 @@
-from pywebpush import webpush, WebPushException
-from .models import Subscription
-
-VAPID_PRIVATE_KEY = "TU_PRIVATE_VAPID_KEY"
-VAPID_CLAIMS = {"sub": "mailto:tu@email.com"}
-
-def send_notification(payload):
-    for sub in Subscription.objects.all():
-        try:
-            webpush(
-                subscription_info={"endpoint": sub.endpoint, "keys": sub.keys},
-                data=payload,
-                vapid_private_key=VAPID_PRIVATE_KEY,
-                vapid_claims=VAPID_CLAIMS
-            )
-        except WebPushException as ex:
-            print("Error enviando:", repr(ex))
+# from pywebpush import webpush, WebPushException
+# from .models import Subscription
+#
+# VAPID_PRIVATE_KEY = "TU_PRIVATE_VAPID_KEY"
+# VAPID_CLAIMS = {"sub": "mailto:tu@email.com"}
+#
+# def send_notification(payload):
+#     for sub in Subscription.objects.all():
+#         try:
+#             webpush(
+#                 subscription_info={"endpoint": sub.endpoint, "keys": sub.keys},
+#                 data=payload,
+#                 vapid_private_key=VAPID_PRIVATE_KEY,
+#                 vapid_claims=VAPID_CLAIMS
+#             )
+#         except WebPushException as ex:
+#             print("Error enviando:", repr(ex))
